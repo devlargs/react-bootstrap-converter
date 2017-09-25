2042e59c885a8f6649dfca34dc70991b633a6a01
Ralph Largo <ralph.largo@volenday.com>
2017-09-25 20:31:29 +0800

    added converting function

--- src/components/converter.js
+++ src/components/converter.js
@@ -2,30 +2,59 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { updateTemplate } from '../actions/template';
+import uuid from 'uuid/v1'
+import JSONPretty from 'react-json-pretty';
 
 class Converter extends Component {
     constructor(props) {
         super(props);
 
         this.convert = this.convert.bind(this);
+        this.rename = this.rename.bind(this);
+    }
+
+    rename(json){
+        // if(Object.keys(json).length != 0){
+        //     Object.keys(json).map((key, idx) => {
+        //         this.rename(key)
+        //     })
+        // }else{
+        //     console.log(json)
+        // }
     }
 
     convert() {
+        var x2js = new X2JS();
+        let json = x2js.xml2js(this.props.template.value);
         
+        if (json) {
+            this.rename(json);
+            return (
+                <div style={{ marginTop: 10 }}>
+                    <JSONPretty id="json-pretty" json={json}></JSONPretty>
+                </div>
+            )
+        } else {
+            return <h3>Enter a valid template value.</h3>
+        }
     }
 
     render() {
-        console.log(this.props)
+        let { updateTemplate, template } = this.props;
         return (
             <div class="form-group">
                 <hr />
+
+                <div>
+                    <textarea class="form-control" value={template.value} rows="5" onChange={(e) => updateTemplate({ value: e.target.value })}></textarea>
+                </div>
+
                 {
-                    <div>
-                        <label for="comment">Enter bootstrap template to be converted:</label>
-                        <textarea class="form-control" value={this.props.template.value} rows="5" onChange={(e) => this.props.updateTemplate({ value: e.target.value })}></textarea>
-                        <button onClick={this.convert} class="btn btn-success pull-right" style={{ marginTop: 10 }}>Convert</button>
-                    </div>
+                    (template.value) ?
+                        this.convert() :
+                        <h3>Please enter template.</h3>
                 }
+
             </div>
         )
     }
